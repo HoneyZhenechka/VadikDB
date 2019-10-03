9014061d9a9e17c9a09dd04f08dc5456ced1402f
Salgspakker <poshash@live.ru>
2019-10-03 17:37:16 +1000

    Add more test

--- test.py
+++ test.py
@@ -7,11 +7,17 @@ tester = logic.Logic()
 def test_1():
     test = tester.query("create table students ( I INT , J CHAR(10) )")
     assert test == "Not Error"
+    test = tester.query("create table students1 ( I INT , J CHAR(10) )")
+    assert test == "Not Error"
+    test = tester.query("create table students2 ( I INT , J CHAR(10) )")
+    assert test == "Not Error"
     test1 = tester.query("create table VADIC ( I INT , J CHAR(10) )")
     assert test1 == "Not Error"
     test2 = tester.query("show create table VADIC")
     assert test2 == "Not Error"
     test4 = tester.query("drop table VADIC")
+    assert test4 == "Not Error"
+    test4 = tester.query("drop table students")
     assert test4 == "Not Error"
 
 
@@ -27,8 +33,12 @@ def test_2():
 def test_3():
     test = tester.query("create table VADIC ( I INT , J CHAR(10) )")
     test == "Not Error"
+    test4 = tester.query("drop table VADIC")
+    assert test4 == "Not Error"
     test2 = tester.query("create table VADIC ( I INT , J CHAR(10) )")
-    assert test2 == "Error"
+    assert test2 == "Not Error"
+    test4 = tester.query("drop table VADIC")
+    assert test4 == "Not Error"
 
 
 def test_4():
@@ -43,7 +53,7 @@ def test_4():
 def test_5():
     test = tester.query("show table ( I INT, J CHAR(10) )")
     assert test == "Error"
-    test = tester.query("create table ( I INT, J CHAR(10) )")
+    test = tester.query("create table  I INT, J CHAR(10) ")
     assert test == "Error"
     test = tester.query("create table ( I INT, J CHAR(10) )")
     assert test == "Error"
@@ -51,7 +61,8 @@ def test_5():
 
 def test_6():
     test = tester.query("create table VADIC ( I INT , J CHAR(10) )")
-    assert test == 'Error'   #change
+    assert test == 'Not Error'
+
 
 def test_7():
     test = tester.query("куевфывсмсы")
@@ -61,14 +72,16 @@ def test_7():
     test = tester.query("")
     assert test == "Error"
 
+
 def test_8():
     test = tester.query("create table VADIC ( I INT , J CHAR(10) )")
-    assert test == "Error"  #change
+    assert test == "Error"
+
 
 def test_9():
     test = tester.query("drop table")
     assert test == "Error"
-    test = tester.query("drop table 123")
+    test = tester.query("drop table not_existing_table")
     assert test == "Error"
     test = tester.query("drop table create table")
     assert test == "Error"
@@ -85,6 +98,54 @@ def test_10():
     assert test == "Error"
     test = tester.query("show create table """)
     assert test == "Error"
+    test = tester.query("show create table show create table")
+    assert test == "Error"
+    test = tester.query("show create table table create")
+    assert test == "Error"
+    test = tester.query("show")
+    assert test == "Error"
+    test = tester.query("create show")
+    assert test == "Error"
+    test = tester.query("ow create table 444")
+    assert test == "Error"
+
+
+def test_11():
+    test = tester.query("create table create table create table TEST_TABLE ( I INT , J CHAR(10) )")
+    assert test == "Error"
+    test = tester.query("create table show create table 123 ( I INT , J CHAR(10) )")
+    assert test == "Error"
+    test = tester.query("create table create table create table TEST_TABLE")
+    assert test == "Error"
+
+
+def test_12():
+    test = tester.query("")
+    assert test == "Error"
+    test = tester.query("empty")
+    assert test == "Error"
+    test = tester.query("create table 123")
+    assert test == "Error"
+
+
+def test_13():
+    test = tester.query("show create table x")
+    assert test == "Error"
+    test = tester.query("show create table students")
+    assert test == "Error"
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
