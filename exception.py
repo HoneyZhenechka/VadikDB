0c406ab7a44a50673e4215857b547b98ecf34dcb
Evgeny Schukin <yevgeny2525@gmail.com>
2019-10-03 14:23:31 +1000

    add spaces

--- exception.py
+++ exception.py
@@ -1,16 +1,16 @@
 class DBNotExists(Exception):
     def __init__(self, db_name):
-        print("Database " + db_name + "not exists!")
+        print("Database " + db_name + " not exists!")
 
 
 class TableAlreadyExists(Exception):
     def __init__(self, table_name):
-        print("Table " + table_name + "already exists!")
+        print("Table " + table_name + " already exists!")
 
 
 class TableNotExists(Exception):
     def __init__(self, table_name):
-        print("Table " + table_name + "not exists!")
+        print("Table " + table_name + " not exists!")
 
 
 class IncorrectSyntax(Exception):
