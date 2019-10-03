bf5519e0a67ff11421b2840e5011200373942a7c
Бондаревский Алексей <bondarevskii.as@gmail.com>
2019-10-03 14:50:23 +1000

    Revert "for revert"
    This reverts commit c49d3968389cb04303ecfa085525f6e3dbbbea3d.

--- engine.py
+++ engine.py
@@ -84,16 +84,13 @@ class DBManager:
         self.__extract_db()
         with open("db_meta.json", "r") as meta_file:
             meta_data = json.load(meta_file)
+            files_tables_list = self.__get_files_tables_list(meta_data)
             if table_name in meta_data["tables"]:
                 self.__is_exception = True
                 exception.TableAlreadyExists(table_name)
             if self.__is_exception:
-<<<<<<< HEAD
                 self.__write_db(files_tables_list)
                 return "ERROR"
-=======
-                return
->>>>>>> parent of 201ba33... remove files in engine
             meta_data["tables"].append(table_name)
         with open("db_meta.json", "w") as meta_file:
             json.dump(meta_data, meta_file)
@@ -116,7 +113,6 @@ class DBManager:
             json.dump(data_json, data_file)
         with open(table_meta_file, "w") as meta_file:
             json.dump(table_meta, meta_file)
-        files_tables_list = self.__get_files_tables_list(meta_data)
         self.__write_db(files_tables_list)
         return "NOT ERROR"
 
@@ -125,16 +121,13 @@ class DBManager:
         self.__extract_db()
         with open("db_meta.json", "r") as table_file:
             meta_data = json.load(table_file)
+            files_tables_list = self.__get_files_tables_list(meta_data)
             if table_name not in meta_data["tables"]:
                 self.__is_exception = True
                 exception.TableNotExists(table_name)
         if self.__is_exception:
-<<<<<<< HEAD
             self.__write_db(files_tables_list)
             return "ERROR"
-=======
-            return
->>>>>>> parent of 201ba33... remove files in engine
         table_meta_file = "table_" + table_name + "_meta.json"
         with open(table_meta_file, "r") as table_file:
             table_meta = json.load(table_file)
@@ -143,7 +136,6 @@ class DBManager:
         for key in fields:
             fields_str += key + " " + fields[key] + ", "
         fields_str = fields_str[:-2]
-        files_tables_list = self.__get_files_tables_list(meta_data)
         self.__write_db(files_tables_list)
         query = (
                 "CREATE TABLE " + table_name + " (\n" +
@@ -162,16 +154,13 @@ class DBManager:
         self.__extract_db()
         with open("db_meta.json", "r") as meta_file:
             meta_data = json.load(meta_file)
+            files_tables_list = self.__get_files_tables_list(meta_data)
             if table_name not in meta_data["tables"]:
                 self.__is_exception = True
                 exception.TableNotExists(table_name)
             if self.__is_exception:
-<<<<<<< HEAD
                 self.__write_db(files_tables_list)
                 return "ERROR"
-=======
-                return
->>>>>>> parent of 201ba33... remove files in engine
             meta_data["tables"].remove(table_name)
         table_meta_file = "table_" + table_name + "_meta.json"
         os.remove(table_meta_file)
@@ -182,7 +171,6 @@ class DBManager:
             data_json.pop(table_name)
         with open("data.json", "w") as data_file:
             json.dump(data_json, data_file)
-        files_tables_list = self.__get_files_tables_list(meta_data)
         self.__write_db(files_tables_list)
         return "NOT ERROR"
 
