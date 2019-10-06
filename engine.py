656a03c8ca903184489f36e95e510324c13b7d12
Evgeny Schukin <yevgeny2525@gmail.com>
2019-10-06 22:11:28 +1000

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,5 +1,4 @@
 import meta_engine
-import exception
 
 
 class DatabaseManager:
@@ -9,17 +8,23 @@ class DatabaseManager:
     def create_table(self, table_name, fields):
         try:
             self.meta_db.create_table(table_name, fields)
-        except exception.TableAlreadyExists:
+        except Exception:
             return "ERROR"
 
     def show_create_table(self, table_name):
         try:
             self.meta_db.show_create_table(table_name)
-        except exception.TableNotExists:
+        except Exception:
             return "ERROR"
 
     def drop_table(self, table_name):
         try:
             self.meta_db.drop_table(table_name)
-        except exception.TableNotExists:
+        except Exception:
+            return "ERROR"
+
+    def insert(self, table_name, fields, values):
+        try:
+            self.meta_db.insert(table_name, fields, values)
+        except Exception:
             return "ERROR"
