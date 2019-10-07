541e10dcf372a5c35c119b173d3ce81c3eb10a32
Evgeny Schukin <yevgeny2525@gmail.com>
2019-10-07 19:03:48 +1000

    rewrite show_create and insert

--- meta_engine.py
+++ meta_engine.py
@@ -8,43 +8,6 @@ from pathlib import Path
 class Database:
     __current_directory = ""
 
-    def __to_bool(self, value):
-        if value.lower() in ("true", "1"):
-            return True
-        if value.lower() in ("false", "0"):
-            return False
-        raise Exception('Invalid value for boolean conversion: ' + value)
-
-    def __check_type(self, table_meta, fields, values):
-        value_index = 0
-        if len(fields) == 0:
-            for key, value in table_meta["fields"].items():
-                if value == "int":
-                    try:
-                        int(values[value_index])
-                    except Exception:
-                        raise exception.InvalidDataType()
-                if value == "bool":
-                    try:
-                        self.__to_bool(values[value_index])
-                    except Exception:
-                        raise exception.InvalidDataType()
-                value_index = value_index + 1
-        else:
-            for field in fields:
-                for key, value in table_meta["fields"].items():
-                    if (value == "int") and (key == field):
-                        try:
-                            int(values[value_index])
-                        except Exception:
-                            raise exception.InvalidDataType()
-                    if (value == "bool") and (key == field):
-                        try:
-                            self.__to_bool(values[value_index])
-                        except Exception:
-                            raise exception.InvalidDataType()
-                    value_index = value_index + 1
-
     def __init__(self, database_name):
         self.__current_directory = Path.cwd()
         db_directory = self.__current_directory / database_name
@@ -88,25 +51,16 @@ class Database:
             json.dump(table_meta, meta_file)
 
     def show_create_table(self, table_name):
-        with open(self.__current_directory / "db_meta.json", "r") as table_file:
-            meta_data = json.load(table_file)
-            if table_name not in meta_data["tables"]:
-                raise exception.TableNotExists(table_name)
-        table_meta_file = "table_" + table_name + "_meta.json"
-        with open(self.__current_directory / table_meta_file, "r") as table_file:
-            table_meta = json.load(table_file)
-        fields = table_meta["fields"]
-        fields_str = ""
-        for key in fields:
-            fields_str += key + " " + fields[key] + ", "
-        fields_str = fields_str[:-2]
+        current_table = table.Table()
+        current_table.load_from_file(table_name, self.__current_directory)
+        fields_str = ", ".join(current_table.table_matrix[0])
         query = (
-                "CREATE TABLE " + table_name + " (\n" +
+                "CREATE TABLE " + current_table.table_name + " (\n" +
                 "\t\t\t\t\t" + fields_str + "\n\t\t\t\t);"
         )
         print(
                 "===================================================\n" +
-                '\t\tTable:\t"' + table_name + '"\n' +
+                '\t\tTable:\t"' + current_table.table_name + '"\n' +
                 "Create Table:\t" + query + "\n" 
                 "==================================================="
         )
@@ -128,46 +82,44 @@ class Database:
             json.dump(data_json, data_file)
 
     def insert(self, table_name, fields, values):
-        with open(self.__current_directory / "db_meta.json", "r") as meta_file:
-            meta_data = json.load(meta_file)
-            if table_name not in meta_data["tables"]:
-                raise exception.TableNotExists(table_name)
-        with open(self.__current_directory / "data.json", "r") as data_file:
-            data_json = json.load(data_file)
-        for field in fields:
-            if field not in data_json[table_name]:
-                raise exception.FieldNotExists(field)
-        table_meta_file = "table_" + table_name + "_meta.json"
-        with open(self.__current_directory / table_meta_file, "r") as table_file:
-            table_meta = json.load(table_file)
-        self.__check_type(table_meta, fields, values)
-        value_index = 0
+        current_table = table.Table()
+        current_table.load_from_file(table_name, self.__current_directory)
+        values_types = []
         if len(fields) == 0:
-            for key in data_json[table_name]:
-                data_json[table_name][key].append(values[value_index])
-                value_index = value_index + 1
+            values_types = current_table.types
         else:
-            for key in data_json[table_name]:
-                if key not in fields:
-                    data_json[table_name][key].append("NULL")
+            for field in fields:
+                if field not in current_table.table_matrix[0]:
+                    raise exception.FieldNotExists(field)
+                values_types.append(current_table.get_type(field))
+        for i in range(len(values)):
+            current_table.check_value(values[i], values_types[i])
+        if len(fields) == 0:
+            current_table.table_matrix.append(values)
+        else:
+            inserted_index = 0
+            inserted_values = []
+            for i in range(len(current_table.table_matrix[0])):
+                if current_table.table_matrix[0][i] not in fields:
+                    inserted_values.append("NULL")
                 else:
-                    data_json[table_name][key].append(values[value_index])
-                    value_index = value_index + 1
-        with open(self.__current_directory / "data.json", "w") as data_file:
-            json.dump(data_json, data_file)
+                    inserted_values.append(values[inserted_index])
+                    inserted_index = inserted_index + 1
+            current_table.table_matrix.append(inserted_values)
+        current_table.save_to_file(self.__current_directory)
 
     def delete(self, table_name, where_field="", where_value=""):
         current_table = table.Table()
         current_table.load_from_file(table_name, self.__current_directory)
         if(where_field == "") and (where_value == ""):
-            for i in range(1, len(current_table.rows), 1):
-                current_table.rows.pop()
+            for i in range(1, len(current_table.table_matrix), 1):
+                current_table.table_matrix.pop()
         else:
-            field_index = current_table.rows[0].index(where_field)
+            field_index = current_table.table_matrix[0].index(where_field)
             i = 1
-            while i < len(current_table.rows):
-                if current_table.rows[i][field_index] == where_value:
-                    current_table.rows.pop(i)
+            while i < len(current_table.table_matrix):
+                if current_table.table_matrix[i][field_index] == where_value:
+                    current_table.table_matrix.pop(i)
                     i = i - 1
                 i = i + 1
         current_table.save_to_file(self.__current_directory)
--- table.py
+++ table.py
@@ -1,13 +1,19 @@
 import json
+import exception
 
 
 class Table:
-    types = []
-    rows = [[]]
-    table_name = ""
-
     def __init__(self):
-        pass
+        self.types = []
+        self.table_matrix = [[]]
+        self.table_name = ""
+
+    def __to_bool(self, value):
+        if value.lower() in ("true", "1"):
+            return True
+        if value.lower() in ("false", "0"):
+            return False
+        raise Exception('Invalid value for boolean conversion: ' + value)
 
     def load_from_file(self, table_name, path):
         self.table_name = table_name
@@ -18,17 +24,32 @@ class Table:
             data_json = json.load(data_file)
         for key, value in table_meta["fields"].items():
             self.types.append(value)
-            self.rows[0].append(key)
-        for i in data_json[table_name][self.rows[0][0]]:
-            self.rows.append([])
+            self.table_matrix[0].append(key)
+        for i in data_json[table_name][self.table_matrix[0][0]]:
+            self.table_matrix.append([])
         temp_list = []
         for key, value in data_json[table_name].items():
             temp_list.append(value)
         for i in range(len(temp_list)):
             records_index = 1
             for j in range(len(temp_list[i])):
-                self.rows[records_index].append(temp_list[i][j])
+                self.table_matrix[records_index].append(temp_list[i][j])
                 records_index = records_index + 1
+
+    def get_type(self, field):
+        return self.types[self.table_matrix[0].index(field)]
+
+    def check_value(self, value, value_type):
+        if value_type == "int":
+            try:
+                int(value)
+            except ValueError:
+                raise exception.InvalidDataType()
+        if value_type == "bool":
+            try:
+                self.__to_bool(value)
+            except Exception:
+                raise exception.InvalidDataType()
 
     def save_to_file(self, path):
         table_meta_file = "table_" + self.table_name + "_meta.json"
@@ -37,16 +58,16 @@ class Table:
         with open(path / "data.json", "r") as data_file:
             data_json = json.load(data_file)
         fields_dict = {}
-        for i in range(len(self.rows[0])):
-            fields_dict[self.rows[0][i]] = self.types[i]
+        for i in range(len(self.table_matrix[0])):
+            fields_dict[self.table_matrix[0][i]] = self.types[i]
         data_dict = {}
         temp_list = []
-        for i in range(len(self.rows[0])):
+        for i in range(len(self.table_matrix[0])):
             temp_list.append([])
-            for j in range(1, len(self.rows), 1):
-                temp_list[i].append(self.rows[j][i])
-        for i in range(len(self.rows[0])):
-            data_dict[self.rows[0][i]] = temp_list[i]
+            for j in range(1, len(self.table_matrix), 1):
+                temp_list[i].append(self.table_matrix[j][i])
+        for i in range(len(self.table_matrix[0])):
+            data_dict[self.table_matrix[0][i]] = temp_list[i]
         table_meta["fields"] = fields_dict
         data_json[self.table_name] = data_dict
         with open(path / table_meta_file, "w") as meta_file:
