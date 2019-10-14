964cdf018e30d78cd3225bb7ede8ada5d7a600a1
Evgeny Schukin <yevgeny2525@gmail.com>
2019-10-14 21:31:23 +1000

    add ROW class

--- engine/db_structure.py
+++ engine/db_structure.py
@@ -32,7 +32,6 @@ class Database:
             table_obj.read_file()
             self.tables.append(table_obj)
 
-
 class Table:
     def __init__(self, file: engine.bin_file.BinFile):
         fields_count = 14
@@ -40,6 +39,9 @@ class Table:
         self.index = -1
         self.name = ""
         self.file = file
+        self.fields = []
+        self.types = []
+        self.positions = {}
         self.size = 32 + 22 + fields_count * 24
 
     def write_file(self):
@@ -47,6 +49,9 @@ class Table:
 
     def read_file(self):
         pass
+
+    def get_fields(self, fields=[], replace=False):
+        return fields
 
 
 class Page:
@@ -84,5 +89,61 @@ class Page:
 
 
 class Row:
+    def __init__(self, table: Table, index=0):
+        self.row_index = index
+        self.table = table
+        self.values = []
+        self.row_id = 0
+        self.previous_index = 0
+        self.next_index = 0
+        self.row_available = 0
+
+    def write_info(self):
+        row_size = self.row_index + self.table.rows_length
+        self.table.file.write_integer(self.row_available, self.row_index, 1)
+        self.table.file.write_integer(self.row_id, self.row_index + 1, 3)
+        self.table.file.write_integer(self.previous_index, row_size - 3, 3)
+        self.table.file.write_integer(self.next_index, row_size - 6, 3)
+
+    def read_info(self):
+        row_size = self.row_index + self.table.rows_length
+        self.row_available = self.table.file.read_integer(self.row_index, 1)
+        self.row_id = self.table.file.read_integer(self.row_index + 1, 3)
+        self.previous_index = self.table.file.read_integer(row_size - 3, 3)
+        self.next_index = self.table.file.read_integer(row_size - 6, 3)
+
+    def drop_row(self):
+        if self.next_index:
+            next_row = Row(self.table, self.next_index)
+            next_row.read_info()
+            next_row.previous_index = self.previous_index
+            next_row.write_info()
+        if self.previous_index:
+            previous_row = Row(self.table, self.previous_index)
+            previous_row.read_info()
+            previous_row.next_index = self.next_index
+            previous_row.write_info()
+
+    def write_row_to_file(self):
+        self.write_info()
+        for value in self.values:
+            value_index = self.table.fields.index(value)
+            value_type = self.table.types[value_index]
+            value_position = self.table.positions[value]
+            self.table.file.write_by_type(value_type, value, self.row_index + value_position, value_type.size)
+
+    def read_row_from_file(self, fields):
+        fields = self.table.get_fields(fields, replace=True)
+        self.read_info()
+        for field, pos in self.table.positions.items():
+            if field not in fields:
+                continue
+            index = self.table.fields.index(field)
+            value_type = self.table.types[index]
+            self.values = []
+            self.values.append(self.table.file.read_by_type(value_type.name, self.row_index + pos, value_type.size))
+
+
+class Type:
     def __init__(self):
         pass
