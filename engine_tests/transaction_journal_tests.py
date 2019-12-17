5b258301bf58810b7c5a016a0995cd9f32a9240d
Evgeny Schukin <yevgeny2525@gmail.com>
2019-12-17 21:26:11 +1000

    rewrite journal tests

--- engine_tests/transaction_journal_tests.py
+++ engine_tests/transaction_journal_tests.py
@@ -4,39 +4,28 @@ db = db_py.Database()
 db.create_table("vadik_table", {"zhenya1": "int", "zhenya2": "str"})
 
 
-def get_block_rows(block):
-    rows_list = []
-    for row in block.iter_rows():
-        if row.row_available == 1:
-            rows_list.append(row)
-    return rows_list
-
-
-def get_all_rows_list():
-    all_rows_list = []
+def get_row_by_id(id):
     for block in db.tables[0].iter_blocks():
-        all_rows_list.append(get_block_rows(block))
-    return all_rows_list
+        for row in block.iter_rows():
+            if (row.row_available == 1) and (row.row_id == id):
+                return row
 
 
 def test_transaction():
     id = db.tables[0].start_transaction()
     db.tables[0].insert(["zhenya1", "zhenya2"], [99, "test_string_123"], transaction_id=id)
     db.tables[0].insert(["zhenya1", "zhenya2"], [992, "test_string_321"], transaction_id=id)
-    rows_list = get_all_rows_list()
-    db.tables[0].update(["zhenya2"], [["lovetsov"]], [rows_list[0][0]], id)
-    db.tables[0].delete([rows_list[0][1].index_in_file], id)
+    db.tables[0].update(["zhenya2"], [["lovetsov"]], [get_row_by_id(0)], id)
+    db.tables[0].delete([get_row_by_id(1).index_in_file], id)
     db.tables[0].end_transaction(id)
-    rows_list = get_all_rows_list()
     assert db.tables[0].count_rows() == 1
-    assert rows_list[0][0].fields_values_dict["zhenya2"] == "lovetsov"
+    assert get_row_by_id(0).fields_values_dict["zhenya2"] == "lovetsov"
 
 
 def test_read_commited():
     id = db.tables[0].start_transaction()
-    rows_list = get_all_rows_list()
-    db.tables[0].update(["zhenya2"], [["anime"]], [rows_list[0][0]], id)
-    result_rows = db.tables[0].select(["zhenya2"], [rows_list[0][0]], id)
+    db.tables[0].update(["zhenya2"], [["anime"]], [get_row_by_id(0)], id)
+    result_rows = db.tables[0].select(["zhenya2"], [get_row_by_id(0)], id)
     db.tables[0].end_transaction(id)
     result_value = result_rows[0].fields_values_dict["zhenya2"]
     assert result_value == "lovetsov"
@@ -49,20 +38,17 @@ def test_rollback():
     db.tables[0].insert(["zhenya1", "zhenya2"], [992, "tesssttttt_string_321"], transaction_id=id)
     db.tables[0].end_transaction(id, True)
     db.tables[0].rollback_transaction(id)
-    rows_list = get_all_rows_list()
     assert db.tables[0].count_rows() == 1
-    assert rows_list[0][0].fields_values_dict["zhenya2"] == "anime"
+    assert get_row_by_id(0).fields_values_dict["zhenya2"] == "anime"
 
 
 def test_wide_rollback():
     id = db.tables[0].start_transaction()
-    rows_list = get_all_rows_list()
-    db.tables[0].update(["zhenya2"], [["xxx"]], [rows_list[0][0]], id)
+    db.tables[0].update(["zhenya2"], [["xxx"]], [get_row_by_id(0)], id)
     db.tables[0].end_transaction(id, True)
     db.close_db()
     db.connect_to_db("zhavoronkov.vdb")
-    rows_list = get_all_rows_list()
-    assert rows_list[0][0].fields_values_dict["zhenya2"] == "anime"
+    assert get_row_by_id(0).fields_values_dict["zhenya2"] == "anime"
 
 
 def test_durability():
